cb5850f9bde7041cccac27a9f300d1b7cc5e4002
mathdu07 <mathdu07.minecraft@gmail.com>
2013-09-23 20:22:47 +0200

    Add plugin's base

---
+++ src/fr/mathdu07/visualshop/VisualShop.java
@@ -0,0 +1,26 @@
+package fr.mathdu07.visualshop;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Main Visual Shop's class
+ */
+public class VisualShop extends JavaPlugin {
+
+
+	@Override
+	public void onDisable() {
+		
+	}
+
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onLoad() {
+		
+	}
+
+}
