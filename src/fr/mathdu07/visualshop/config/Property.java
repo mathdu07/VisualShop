3c6af1f759a4b960953c0806f3d4bc83dc87dc98
mathdu07 <mathdu07.minecraft@gmail.com>
2013-10-23 17:50:20 +0200

    Set property's field to public

--- src/fr/mathdu07/visualshop/config/Property.java
+++ src/fr/mathdu07/visualshop/config/Property.java
@@ -7,42 +7,14 @@ package fr.mathdu07.visualshop.config;
  */
 public class Property<T> {
 	
-	private final String key;
-	private T value;
-	private final T defaultValue;
+	public final String key;
+	public T value;
+	public final T defaultValue;
 	
 	public Property (String key, T defaultValue) {
 		this.key = key;
 		this.defaultValue = defaultValue;
 		this.value = defaultValue;
-	}
-
-	/**
-	 * @return the value of the property
-	 */
-	public T getValue() {
-		return value;
-	}
-
-	/**
-	 * @param value - the value to set
-	 */
-	public void setValue(T value) {
-		this.value = value;
-	}
-
-	/**
-	 * @return the key
-	 */
-	public String getKey() {
-		return key;
-	}
-
-	/**
-	 * @return the defaultValue
-	 */
-	public T getDefaultValue() {
-		return defaultValue;
 	}
 
 }
