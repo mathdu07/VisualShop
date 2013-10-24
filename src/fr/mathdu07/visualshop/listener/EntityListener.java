53722f0de51869b07bc9f16024415f4579cedd08
mathdu07 <mathdu07.minecraft@gmail.com>
2013-10-24 17:45:29 +0200

    Improve shop protection

--- src/fr/mathdu07/visualshop/listener/EntityListener.java
+++ src/fr/mathdu07/visualshop/listener/EntityListener.java
@@ -3,10 +3,14 @@ package fr.mathdu07.visualshop.listener;
 import java.util.List;
 
 import org.bukkit.block.Block;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityChangeBlockEvent;
+import org.bukkit.event.entity.EntityCombustEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.entity.ExplosionPrimeEvent;
 import org.bukkit.event.entity.ItemDespawnEvent;
 
 import fr.mathdu07.visualshop.Shop;
@@ -36,9 +40,34 @@ public class EntityListener implements Listener {
 			for (int i = 0; i < blocks.size(); i++) {
 				
 				if (Shop.hasShopAt(blocks.get(i).getLocation())) {
-					e.setCancelled(true);
-					//TODO Find a way to cancel destruction of block only concerned
-					return;
+					if (VisualShop.getVSConfig().getBooleanProperty(Config.PROTECT_SHOPS)) {
+						e.setCancelled(true);
+						break;
+					}
+					else
+						Shop.deleteShop(blocks.get(i).getLocation());
+				}
+			}
+		}
+	}
+	
+	@EventHandler
+	public void onEntityPrime (ExplosionPrimeEvent e){
+		if (e == null)	return;
+		
+		final Entity ent = e.getEntity();
+		
+		if (VisualShop.getVSConfig().getBooleanProperty(Config.PROTECT_SHOPS)) {
+			
+			for (Entity entity : ent.getNearbyEntities(e.getRadius(), e.getRadius(), e.getRadius())) {
+				
+				if (entity instanceof Item) {
+					Item i = (Item) entity;
+					
+					if (Shop.isItemOwnedToAShop(i)) {
+						e.setCancelled(true);
+						break;
+					}
 				}
 			}
 		}
